5b838474ef82f43a5aecf892aad231ebf5bf051f
Rahima <rahimaaa3024@gmail.com>
2026-09-26 02:13:52 +0600

    added logic for only 5 workouts can be selected

--- src/components/plandetails/AddTodaysPlan.jsx
+++ src/components/plandetails/AddTodaysPlan.jsx
@@ -11,18 +11,14 @@ const AddTodaysPlan = ({ workout }) => {
     const isAdded = todaysWorkout.find((item) => item.id === workout.id);
 
     const handleAddToTodaysPlan = () => {
-        // if (todaysWorkout.length < 5) {
-        //     setTodaysWorkout((prev) =>
-        //         prev.find((item) => item.id === workout.id) ? prev : [...prev, workout]
-        //     );
-        //     toast.success("Added to today's plan");
-        // } else {
-        //     toast.error("You can only add 5 workouts per day.");
-        // }
-        setTodaysWorkout((prev) =>
-            prev.find((item) => item.id === workout.id) ? prev : [...prev, workout]
-        );
-        toast.success("Added to today's plan");
+        if (todaysWorkout.length < 5) {
+            setTodaysWorkout((prev) =>
+                prev.find((item) => item.id === workout.id) ? prev : [...prev, workout]
+            );
+            toast.success("Added to today's plan");
+        } else {
+            toast.error("You can only add 5 workouts per day.");
+        }
     }
 
     return (
